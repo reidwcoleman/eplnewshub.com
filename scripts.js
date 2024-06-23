71fe932213029210a394ee642ec545aa741fce7f
reidwcoleman <reidwcoleman@gmail.com>
2024-06-23 16:40:21 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,9 +5,23 @@ let votes = {
     'Team D': 0
 };
 
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (getCookie('voted') === 'true') {
+        disableButtons();
+        displayResults();
+    }
+});
+
 function vote(team) {
+    if (getCookie('voted') === 'true') {
+        alert('You have already voted!');
+        return;
+    }
+    
     votes[team]++;
+    setCookie('voted', 'true', 30); // Cookie valid for 30 days
     displayResults();
+    disableButtons();
 }
 
 function displayResults() {
@@ -18,4 +32,32 @@ function displayResults() {
         resultsHTML += `<p>${team}: ${votes[team]} vote(s) (${percentage}%)</p>`;
     }
     document.getElementById('poll-results').innerHTML = resultsHTML;
+}
+
+function disableButtons() {
+    const buttons = document.querySelectorAll('#poll-options button');
+    buttons.forEach(button => {
+        button.disabled = true;
+    });
+}
+
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
 }
